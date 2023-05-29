332bf1c33a8d037ae6b87084b7ea79aa057c0208
Lalord22 <lalo22296@hotmail.com>
2023-05-28 21:53:01 -0600

    Cambio minimo a las etiquetas de coberturas.js

--- JEGEDAsegurosBackEnd/src/main/webapp/js/coberturas.js
+++ JEGEDAsegurosBackEnd/src/main/webapp/js/coberturas.js
@@ -1,17 +1,17 @@
 class Coberturas {
-    constructor() {
-        this.state = {
-            entities: [], // Initialize entities as an empty array
-            mode: '', // Initialize mode
-        };
-        this.dom = this.render();
-        this.modal = new bootstrap.Modal(this.dom.querySelector('#modal'));
-        this.dom.querySelector("#create").addEventListener('click', () => this.makenew()); // Use arrow function to maintain the context
-        this.dom.querySelector('#apply').addEventListener('click', () => this.add()); // Use arrow function to maintain the context
-    }
+  constructor() {
+    this.state = {
+      entities: [], // Initialize entities as an empty array
+      mode: '', // Initialize mode
+    };
+    this.dom = this.render();
+    this.modal = new bootstrap.Modal(this.dom.querySelector('#modal'));
+    this.dom.querySelector("#create").addEventListener('click', () => this.makenew());
+    this.dom.querySelector('#apply').addEventListener('click', () => this.add());
+  }
 
-    render() {
-        const html = `
+  render() {
+    const html = `
       <div id="coberturas">
         <div id="list" class="container">
           <div class="card bg-light">
@@ -37,7 +37,6 @@ class Coberturas {
                       <th scope="col">Descripcion</th>
                       <th scope="col">Precio Minimo</th>
                       <th scope="col">Precio Porcentual</th>
-
                     </tr>
                   </thead>
                   <tbody id="listbody"></tbody>
@@ -55,7 +54,7 @@ class Coberturas {
               <span style='margin-left:4em;font-weight: bold;'>Marcas</span>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form id="form">
+            <form id="modalForm">
               <div class="modal-body">
                 <h1>To do</h1>
               </div>
@@ -67,60 +66,58 @@ class Coberturas {
         </div>
       </div>
     `;
-        const coberturasContainer = document.createElement('div');
-        coberturasContainer.innerHTML = html;
-        return coberturasContainer;
-    }
+    const coberturasContainer = document.createElement('div');
+    coberturasContainer.innerHTML = html;
+    return coberturasContainer;
+  }
 
-    list() {
-        const request = new Request(`${backend}/coberturas`, {method: 'GET', headers: {}});
-        (async () => {
-            try {
-                const response = await fetch(request);
-                if (!response.ok) {
-                    errorMessage(response.status);
-                    return;
-                }
-                const coberturas = await response.json();
-                this.state.entities = coberturas; // Update entities in the state
-                const listing = this.dom.querySelector("#listbody");
-                listing.innerHTML = "";
-                this.state.entities.forEach(e => this.row(listing, e));
-            } catch (error) {
-                console.error('Error fetching modelos:', error);
-            }
-        })();
-    }
+  list() {
+    const request = new Request(`${backend}/coberturas`, { method: 'GET', headers: {} });
+    (async () => {
+      try {
+        const response = await fetch(request);
+        if (!response.ok) {
+          errorMessage(response.status);
+          return;
+        }
+        const coberturas = await response.json();
+        this.state.entities = coberturas; // Update entities in the state
+        const listing = this.dom.querySelector("#listbody");
+        listing.innerHTML = "";
+        this.state.entities.forEach(e => this.row(listing, e));
+      } catch (error) {
+        console.error('Error fetching modelos:', error);
+      }
+    })();
+  }
 
-    row(list, co) {
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
+  row(list, co) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
       <td>${co.id}</td>
       <td>${co.descripcion}</td>
       <td>${co.costoMinimo}</td>
       <td>${co.costoPorcentual}</td>`;
-        list.append(tr);
-    }
+    list.append(tr);
+  }
 
-    makenew() {
-        this.reset();
-        this.state.mode = 'A'; // Adding
-        this.showModal();
-    }
+  makenew() {
+    this.reset();
+    this.state.mode = 'A'; // Adding
+    this.showModal();
+  }
 
-    add() {
-        // TODO: Validate data, load into entity, invoke backend for adding
-        this.list();
-        this.reset();
-        this.modal.hide();
-    }
+  add() {
+    // TODO: Validate data, load into entity, invoke backend for adding
+    this.list();
+    this.reset();
+    this.modal.hide();
+  }
 
-    // Other methods (load, reset, emptyEntity, update, validate) can be added here
+  // Other methods (load, reset, emptyEntity, update, validate) can be added here
 }
 
 // Usage example:
 const coberturasTable = new Coberturas();
 document.body.appendChild(coberturasTable.dom);
-Table.list(); // Call list() to fetch and display the polizas
-
-
+coberturasTable.list(); // Call list() to fetch and display the polizas
